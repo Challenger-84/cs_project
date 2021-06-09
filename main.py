034d59840096ba9fdffd8681ff7d237351f606f8
Eshwar <reshwar2004@gmail.com>
2021-06-10 00:08:12 +0530

    added route to login and signup page

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello_world():
    return "Hello World"
 
+@app.route('/login')
+def login():
+    return 'this is the login page'
+
+@app.route('/signup')
+def signup():
+    return 'this is the signup page'
+
 if __name__ == '__main__':
    app.run()

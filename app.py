a1c5c9b416eb516cc81620417d3e36cfb9bbe5c7
Eshwar <reshwar2004@gmail.com>
2021-06-16 14:27:48 +0530

    Added Base template which all pages can inherit from

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return "Hello World"
+app.config['MYSQL_HOST'] = 'freedb.tech'
+app.config['MYSQL_USER'] = 'freedbtech_Eshwar'
+app.config['MYSQL_PASSWORD'] = 'csproject'
+app.config['MYSQL_DB'] = 'freedbtech_CsProject'
 
+
+@app.route('/')
+def hello_world():  
+   return render_template('index.html')
+
+@app.route('/')
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -14,4 +23,4 @@ def signup():
     return render_template('sign_up.html')
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)

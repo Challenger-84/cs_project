0a95bdd26a0bdbe6e4eba1673c571fce93b0aa1c
Eshwar <reshwar2004@gmail.com>
2021-06-17 19:15:11 +0530

    Added sessions and a bit of CSS

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
-from flask import Flask,render_template, url_for
-from flask_mysqldb import MySQL
+from flask import Flask,render_template, url_for, redirect, request, session
+from flask_mysql_connector import MySQL
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'freedb.tech'
-app.config['MYSQL_USER'] = 'freedbtech_Eshwar'
-app.config['MYSQL_PASSWORD'] = 'csproject'
-app.config['MYSQL_DB'] = 'freedbtech_CsProject'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DATABASE'] = 'sys'
+mysql = MySQL(app)
 
+app.secret_key = "Veryvery secret key :). ha"
 
 @app.route('/')
 def home():  
-   return render_template('index.html', login_link=url_for('login'), signup_link = url_for('signup') )
+   return render_template('index.html', 
+            login_link = url_for('login'),
+            signup_link = url_for('signup')
+        )
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        session['username'] = username
+        return redirect(url_for('profile'))
+    else:
+        return render_template('login.html',
+                homepage_link = url_for('home')
+            )
 
 @app.route('/signup')
 def signup():
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', 
+            homepage_link = url_for('home')
+        )
+
+@app.route('/profile')
+def profile():
+    if 'username' in session:
+        username = session['username']
+        return render_template('profile.html', user=username, homepage_link=url_for('home'))
+    else:
+        return redirect('login')
+
 
 if __name__ == '__main__':
    app.run(debug=True)

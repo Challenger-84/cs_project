52fa742c7908821ffb86de681eb74704856801c2
Gladiator3015 <shankarnarayanan21630@gmail.com>
2021-07-14 19:43:25 +0530

    message

--- db_queries.py
+++ db_queries.py
@@ -32,5 +32,8 @@ def password_hash_returner(conn: mysql.connection, username, password):
     output = cursor.fetchone()
     return output[0]
 
-def add_dress(conn: mysql.connection):
-    pass
+def add_values(conn:mysql.connection,username,password):
+    cursor=conn.cursor()
+    query=f'INSERT INTO dress(dressid,name,description,img_url,price,stock)VALUES("{dressid}",{name}","{img_url}","{price}","{stock}")'
+    cursor.execute(query)
+    conn.commit()

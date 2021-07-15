b1fb90993f9e29f2156556109fcd39c15ca98b32
Eshwar <reshwar2004@gmail.com>
2021-07-15 09:04:30 +0530

    changed query

--- db_queries.py
+++ db_queries.py
@@ -45,9 +45,9 @@ def see_all_dress(conn:mysql.connection):
     output=cursor.fetchall()
     return output
 
-def update_user_account(conn:mysql.connection,username,password):
+def update_user_account(conn:mysql.connection, userid, username, new_account_type):
     cursor=conn.cursor()
-    query='UPDATE users SET account_type="{username}","{password}")where userid={userid} and username={username})'
+    query=f'UPDATE users SET account_type="{new_account_type}" where userid="{userid}" and username="{username}"'
     cursor.execute(query)
     conn.commit()
 

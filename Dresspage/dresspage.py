740e4e7b8520b3db3fc4c14bca5abab6e9799d79
Gladiator3015 <shankarnarayanan21630@gmail.com>
2021-08-13 08:59:29 +0530

    changes added

--- Dresspage/dresspage.py
+++ Dresspage/dresspage.py
@@ -1,8 +1,13 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,current_app 
+from db_queries import getDress
 dresspage_blueprint = Blueprint('dresspage', __name__, template_folder='templates', static_folder='static')
 
 @dresspage_blueprint.route('/dresspage/<dressid>')
 def dresspage(dressid):
+    mysql = current_app.config['mysql']
+    connection=mysql.connection
+    dress_info=getDress(connection,dressid)
+    print(dress_info)
     return render_template('Dresspage.html',
                             dress_image_link= 'https://www.nike.com/in/t/sportswear-t-shirt-dw59Bv')
       

54ac4b85c6d26859461f7889c6df38ee579ab6c3
Eshwar <reshwar2004@gmail.com>
2021-08-13 08:41:39 +0530

    Added variables to dresspage html page

--- Dresspage/dresspage.py
+++ Dresspage/dresspage.py
@@ -1,8 +1,8 @@
 from flask import Blueprint,render_template
 dresspage_blueprint = Blueprint('dresspage', __name__, template_folder='templates', static_folder='static')
 
-@dresspage_blueprint.route('/dresspage')
-def dresspage():
+@dresspage_blueprint.route('/dresspage/<dressid>')
+def dresspage(dressid):
     return render_template('Dresspage.html',
                             dress_image_link= 'https://www.nike.com/in/t/sportswear-t-shirt-dw59Bv')
       

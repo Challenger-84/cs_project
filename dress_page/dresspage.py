0722bb02b0f70e6bd907c88445d8667b9bd0c52d
Eshwar <reshwar2004@gmail.com>
2021-09-14 22:58:21 +0530

    Load files from dropbox

--- dress_page/dresspage.py
+++ dress_page/dresspage.py
@@ -1,19 +1,23 @@
-from flask import Blueprint,render_template,current_app
-from flask.helpers import url_for 
-from utils.db_queries import getDress
+from flask import Blueprint,render_template,current_app, url_for
 
-dresspage_blueprint = Blueprint('dresspage', __name__, template_folder='templates', static_folder='static')
+from utils.db_queries import getDress
+from utils.img_host import get_file
+
+dresspage_blueprint = Blueprint('dresspage', __name__, template_folder='templates',static_folder='static')
 
 @dresspage_blueprint.route('/dresspage/<dressid>')
 def dresspage(dressid):
     mysql = current_app.config['mysql']
     connection=mysql.connection
     dress_info=getDress(connection,dressid)
+
+    img_path = dress_info[3]
+    img_path = get_file(img_path)
     
     return render_template('dresspage.html',
                             homepage_link = url_for('home'),
                             dress_name=dress_info[1],
                             description=dress_info[2],
-                            dress_image_link=dress_info[3])
+                            dress_image_link=url_for('.static', filename=img_path))
       
 
--- utils/img_host.py
+++ utils/img_host.py
@@ -1,11 +1,18 @@
 import dropbox
 
-dropbox_access_token = "sl.A4cBU2j37MplLAoqt_6RQm6GRxKcHnaszQPQN1Lb1iFW4wpD1jAOYaK8ueiLiNcFR518XV67oP-UpaHY2xzOcSAgFCGOmUujDGtnv6JiNOJcQBj4B6TTY2vbV6gH0R1fUD5dArM"
+dropbox_access_token = "upKK1SR_CWcAAAAAAAAAAT68jYjGN_cZUkqpBAN4YMug1A5e3qsMZsa8ykikE-2Y"
 
 client = dropbox.Dropbox(dropbox_access_token)
 
 def upload_file(file):
-    dropbox_path = file
+    dropbox_path = '/' + file
     client.files_upload(open(file, 'rb').read(), dropbox_path)
     return file
+
+def get_file(path):
+    with open('static/cache/' + path, "wb") as f:
+        metadata, result = client.files_download(path='/'+ path)
+        f.write(result.content)
+    
+    return 'cache/' + path
 

da9270974c593a774827aa9d6563b7fc0101f354
Eshwar <reshwar2004@gmail.com>
2021-09-15 13:04:20 +0530

    Added a comment

--- utils/img_host.py
+++ utils/img_host.py
@@ -15,6 +15,7 @@ def get_file(path):
     dropbox_access_token = os.getenv('DROPBOX_TOKEN')
     client = dropbox.Dropbox(dropbox_access_token)
 
+    # Gets stored in cache folder which will be cleared every 24 hours
     if not Path('static/cache/' + path).is_file():
         with open('static/cache/' + path, "wb") as f:
             metadata, result = client.files_download(path='/'+ path)

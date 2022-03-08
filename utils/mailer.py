4025c3bd1eb03b91a401349bd48f74048063edc9
Eshwar <reshwar2004@gmail.com>
2022-03-08 19:27:26 +0530

    bug fixes

--- utils/mailer.py
+++ utils/mailer.py
@@ -8,12 +8,11 @@ load_dotenv()
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASS')
 
-smtp =  smtplib.SMTP('smtp.gmail.com', 587)
-smtp.ehlo()
-smtp.starttls()
-smtp.ehlo()
-smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
 def send_mail(reciever, subject, body):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
     msg = f'Subject: {subject}\n\n {body}'
     smtp.sendmail(EMAIL_ADDRESS, reciever, msg)
